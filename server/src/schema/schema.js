7eca1f907ec1891367ac8352dde473c241b49035
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-11-30 01:14:01 +0530

    feat(server): add schema for Track

---
+++ server/src/schema/schema.js
@@ -0,0 +1,32 @@
+import { gql } from 'graphql'
+
+export const typeDefs = gql`
+  type Query {
+    "Query to get the list of trackList for home screen"
+    tracksForHome: [Track!]!
+  }
+
+  "Type is group of modules which has a specific topic to be learned"
+  type Track {
+    id: ID!
+    "Tracks's title"
+    title: String!
+    "Tracks's author's name"
+    author: Author!
+    "Track's thumbnail"
+    thumbnail: String
+    "Length of the track"
+    length: Int
+    "Number of modules in the track"
+    modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile pic URL"
+    photo: String
+  }
+`
