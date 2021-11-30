e0d6e0ef44d7b98e9392ad97be67d2a98e3f0e4e
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-01 00:40:17 +0530

    fix: export schema

--- server/src/schema/schema.js
+++ server/src/schema/schema.js
@@ -1,6 +1,6 @@
-import { gql } from 'graphql'
+import { gql } from 'apollo-server'
 
-export const typeDefs = gql`
+const typeDefs = gql`
   type Query {
     "Query to get the list of trackList for home screen"
     tracksForHome: [Track!]!
@@ -30,3 +30,5 @@ export const typeDefs = gql`
     photo: String
   }
 `
+
+export default typeDefs
