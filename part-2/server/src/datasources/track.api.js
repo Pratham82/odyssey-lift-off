1dd24f38f19d1352a3ef4ef836742a062ea9abb3
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-04 22:29:19 +0530

    feat: added data source

---
+++ part-2/server/src/datasources/track.api.js
@@ -0,0 +1,14 @@
+import {RESTDataSource} from 'apollo-datasource-rest'
+
+export class TrackAPI extends RESTDataSource{
+  constructor(){
+    super()
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/'
+  }
+  getTracksForHome(){
+    return this.get('tracks')
+  }
+  getAuthors(authorId){
+    return this.get(`authors/${authorId}`)
+  }
+}
