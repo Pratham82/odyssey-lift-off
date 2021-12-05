4d7f4b0c1898e7fb4f4d644220a6d8d0cd17d113
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-05 23:56:55 +0530

    fix: fixed author method

--- part-2/server/src/datasources/track.api.js
+++ part-2/server/src/datasources/track.api.js
@@ -1,14 +1,14 @@
-import {RESTDataSource} from 'apollo-datasource-rest'
+import { RESTDataSource } from 'apollo-datasource-rest'
 
-export class TrackAPI extends RESTDataSource{
-  constructor(){
+export class TrackAPI extends RESTDataSource {
+  constructor() {
     super()
     this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/'
   }
-  getTracksForHome(){
+  getTracksForHome() {
     return this.get('tracks')
   }
-  getAuthors(authorId){
-    return this.get(`authors/${authorId}`)
+  getAuthors(authorId) {
+    return this.get(`author/${authorId}`)
   }
 }
